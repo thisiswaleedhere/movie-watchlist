1bf90e5b84f30f29ef75fd147a943bb25e4fbbf2
thisiswaleedhere <thisiswaleedhere@gmail.com>
2022-02-22 22:01:51 +0530

    Started working on Title Card Component

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <div className='text-lg text-white font-sans space-x-8 text-center flex-auto mb-14'>
+        <div className='text-lg text-white font-sans space-x-16 text-center flex-auto mb-14'>
             <button
                 className='bg-[#383737] hover:bg-blue-700 py-2 px-6 rounded-3xl'>
                 Movie List
--- src/TitleCard.js
+++ src/TitleCard.js
@@ -2,7 +2,24 @@ import React from "react";
 
 function TitleCard() {
     return (
-        <div className="mx-32">Div</div>
+        <div className="flex justify-around">
+            <div>
+                <div className="w-20">
+                    <img src="images/Marcus-Rashford.png" alt="Movie Poster" />
+                </div>
+            </div>
+            <div>
+                <div className="grid justify-start grid-cols-4 gap-4 font-sansserif">
+                    <div className="col-span-4 text-xl">Marcus Rashford - The Movie</div>
+                    <div>2023</div>
+                    <div>Documentary</div>
+                    <div>English</div>
+                    <div>Netflix</div>
+
+                </div>
+            </div>
+            <div className="mr-0">1-x</div>
+        </div>
     )
 }
 
