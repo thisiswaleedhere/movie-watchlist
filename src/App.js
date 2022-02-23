8ff70e851c6dd467d04fa31d3b2646d78f8d7a8f
thisiswaleedhere <thisiswaleedhere@gmail.com>
2022-02-23 08:14:13 +0530

    props added to Title Card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
     <body className="bg-white">
       <HeaderBar />
       <NavBar />
-      <TitleCard />
+      <TitleCard title="Marcus Rashford - The Movie" year="2023" lang="English" genre="Biography" />
+      <TitleCard title="newererr namded xyzzz" year="1999" lang="English" genre="Biography" />
+      <TitleCard title="kdjskdjskdjskdjskdjkdjskdjskdjskdjskdj" year="2003" lang="English" genre="Biography" />
+      <TitleCard title="kdjs" year="2021" lang="English" genre="Biography" />
+      <TitleCard title="kdwwwwwwwwwwwwwwwwwwwwww jskdjskdjskdjskdjkdjskdjskdjskdjskdj" year="2023" lang="English" genre="Biography" />
     </body>
   );
 }
--- src/TitleCard.js
+++ src/TitleCard.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-function TitleCard() {
+function TitleCard(props) {
     return (
-        <div className="flex justify-around">
+        <div className="flex justify-center items-center mb-5">
             <div>
-                <div className="w-20">
+                <div className="w-20 mx-16">
                     <img src="images/Marcus-Rashford.png" alt="Movie Poster" />
                 </div>
             </div>
-            <div>
-                <div className="grid justify-start grid-cols-4 gap-4 font-sansserif">
-                    <div className="col-span-4 text-xl">Marcus Rashford - The Movie</div>
-                    <div>2023</div>
-                    <div>Documentary</div>
-                    <div>English</div>
-                    <div>Netflix</div>
+            <div className="ml-12 w-96">
+                <div className="grid justify-items-stretch grid-cols-4 gap-4 font-sansserif">
+                    <div className="col-span-4 text-2xl font-bold">{props.title}</div>
+                    <div>{props.year}</div>
+                    <div>{props.lang}</div>
+                    <div>{props.genre}</div>
+                    {/* <div>Netflix</div> */}
 
                 </div>
             </div>
-            <div className="mr-0">1-x</div>
+            <div className="mx-16 px-10 font-mono text-2xl text-gray-400 hover:text-red-600 hover:font-black hover:cursor-pointer">X</div>
         </div>
     )
 }
