0e092cea3c8129f368a8c66f4bb8f9630815d84b
thisiswaleedhere <thisiswaleedhere@gmail.com>
2022-03-16 17:53:02 +0530

    Added UseState and HanleChange

--- src/AddWidget.js
+++ src/AddWidget.js
@@ -2,10 +2,31 @@ import React from "react";
 
 function AddWidget() {
 
+    const [formData, setFormData] = React.useState(
+        {
+            movieTitle: "",
+            movieYear: "",
+            movieLanguage: "",
+            movieGenre: ""
+        }
+    )
+
+    console.log(formData)
+
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
     function handleSubmit(event) {
         event.preventDefault()
         console.log("Submitted")
     }
+
     return (
         <div className="center bg-gray-500 mx-16 py-8 mb-10 rounded-lg">
 
@@ -21,6 +42,8 @@ function AddWidget() {
                             type="text"
                             placeholder="Title"
                             name="movieTitle"
+                            onChange={handleChange}
+
                         />
                     </div>
 
@@ -34,6 +57,7 @@ function AddWidget() {
                             max="2023"
                             placeholder="Year"
                             name="movieYear"
+                            onChange={handleChange}
                         />
                     </div>
 
@@ -46,6 +70,7 @@ function AddWidget() {
                             type="text"
                             placeholder="Language"
                             name="movieLanguage"
+                            onChange={handleChange}
                         />
                     </div>
 
@@ -58,18 +83,19 @@ function AddWidget() {
                             type="text"
                             placeholder="Genre"
                             name="movieGenre"
+                            onChange={handleChange}
                         />
                     </div>
 
                 </div>
 
-                <div className="w-28 ml-4">
+                {/* <div className="w-28 ml-4">
                     <label htmlFor="movie-image" className="text-white font-sans text-lg">Movie Image:</label>
                     <img src="images/Marcus-Rashford.png" alt="Movie Poster" id="movie-image" />
-                </div>
+                </div> */}
 
                 <button
-                    className="bg-blue-400 hover:bg-blue-700 my-20 ml-4 py-1 px-6 rounded-3xl hover:text-white">
+                    className="bg-blue-400 hover:bg-blue-700 my-20 ml-16 py-1 px-6 rounded-3xl hover:text-white">
                     Add Movie
                 </button>
 
